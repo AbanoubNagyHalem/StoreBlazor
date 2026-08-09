61958d629184f0200a8b73ac65798ff7f70c21a8
Abanoub Nagy <96650563+AbanoubNagyHalem@users.noreply.github.com>
2026-08-09 23:00:33 +0300

    refactor: split product UI into reusable components and improve layout

--- obj/Debug/net10.0/StoreBlazor.AssemblyInfo.cs
+++ obj/Debug/net10.0/StoreBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6140c90db34227d5bc4c81caa30ed6470bb877ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+424492b536f6c3ea1df3e0cba4d20e64e7956500")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
